f5ce3596a6dacaacd6bf88f8098558c9432c1768
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2015-07-27 00:50:12 -0300

    Created tests for Prompt's subclasses. Created tests for FileWriter's subclasses.

--- tests/unit/voxforge2sphinx/corpus_test.py
+++ tests/unit/voxforge2sphinx/corpus_test.py
@@ -33,9 +33,53 @@ class CorpusTest(unittest.TestCase):
         mock_glob.assert_called_with(path.join('file_path', '*.ext'))
 
 
+class FileWriterTest(unittest.TestCase):
+
+    def test_store(self):
+        file_path = 'testpath'
+        file_w = cps.FileWriter(file_path)
+        self.assertEqual(file_w.file, file_path)
+        text = 'Texto'
+        file_w.content.append(text)
+        mock_open = mk.mock_open()
+        with mk.patch('voxforge2sphinx.corpus.open', mock_open, create=True):
+            file_w.store()
+        handle = mock_open()
+        handle.write.assert_called_with(text + '\n')
+
+    def test_add_content(self):
+        text = 'text1 text2'.split()
+        trans = cps.TranscriptionFile('test')
+        trans.add_content(*text)
+        self.assertEqual(trans.content[0], cps.TranscriptionFile.FORMAT.format(*text))
+        fileid = cps.FileidFile('test')
+        fileid.add_content(*text)
+        self.assertEqual(fileid.content[0], cps.FileidFile.FORMAT.format(*text))
+
+
+
 class PromptsTest(unittest.TestCase):
-    def setUp(self):
-        pass
+
+    @mk.patch('voxforge2sphinx.corpus.Corpus.track_files', return_value=['t.txt'])
+    def test_prompts(self, mock_track_files):
+        with self.assertRaises(NotImplementedError):
+            cps.Prompts()
+        with self.assertRaisesRegex(TypeError, 'Missing positional argument'):
+            cps.Prompts.create_prompts()
+        with self.assertRaisesRegex(TypeError, 'Missing.*"multi_path"'):
+            cps.Prompts.create_prompts('')
+
+        with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data='\n'), create=True):
+            with mk.patch('voxforge2sphinx.corpus.path.exists', return_value=False):
+                multi_prompt = cps.Prompts.create_prompts('', multi_path='')
+                self.assertIsInstance(multi_prompt, cps.MultiFilePrompts)
+                self.assertEqual(len(multi_prompt), 0)
+
+        with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(), create=True):
+            with mk.patch('voxforge2sphinx.corpus.path.exists', return_value=True):
+                single_prompt = cps.Prompts.create_prompts('', multi_path='')
+                self.assertIsInstance(single_prompt, cps.SingleFilePrompts)
+                self.assertEqual(len(single_prompt), 0)
 
     def test_valid_single(self):
         data = "094    Vou tomar um pouquinho d'água. \n095 Para onde a senhora quer ir? \n" + \
@@ -43,14 +87,13 @@ class PromptsTest(unittest.TestCase):
         exp_dict = {'094': "vou tomar um pouquinho d'água.", '095': 'para onde a senhora quer ir?',
                     '096': 'que horas são?', '097': 'amanhã é sexta.', '16': 'para onde a senhora quer ir?'}
 
-        with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data=data), create=True) as m:
-            self.assertEqual(cps.SingleFilePrompts(''), exp_dict, 'Dictionary of prompts were loaded incorrectly.')
+        with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data=data), create=True):
+            self.assertEqual(cps.SingleFilePrompts(''), exp_dict)
 
     def test_invalid_single(self):
         data = "094   \nPara onde a senhora quer ir?\n096Que horas são?\n\n"
-        with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data=data),
-                        create=True) as m:
-            self.assertEqual(cps.SingleFilePrompts(''), {}, 'Dictionary of prompts were loaded incorrectly.')
+        with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data=data), create=True):
+            self.assertEqual(cps.SingleFilePrompts(''), {})
 
     @mk.patch('voxforge2sphinx.corpus.Corpus.track_files', return_value=['/home/user/test.txt'])
     @mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data="Vou tomar um pouquinho d'água"), create=True)
