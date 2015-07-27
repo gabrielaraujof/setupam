d5380ee3248f821684bab4d06525908b5f651313
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2015-07-27 14:11:36 -0300

    Added tests for Audios class. Added testes for Speaker.gather_audios (which will be later removed from Speaker class)

--- tests/unit/voxforge2sphinx/corpus_test.py
+++ tests/unit/voxforge2sphinx/corpus_test.py
@@ -26,15 +26,26 @@ import voxforge2sphinx.corpus as cps
 
 
 class CorpusTest(unittest.TestCase):
-
     @mk.patch('voxforge2sphinx.corpus.glob.glob')
     def test_track_files(self, mock_glob):
-        cps.Corpus.track_files('file_path', 'ext')
+        cps.track_files('file_path', 'ext')
         mock_glob.assert_called_with(path.join('file_path', '*.ext'))
 
 
-class FileWriterTest(unittest.TestCase):
+class SpeakerTest(unittest.TestCase):
+    def setUp(self):
+        self.src_path = '/home/user/source'
+        self.speaker = cps.Speaker(self.src_path)
 
+    def test_gather_audios(self):
+        return_list = ('/home/001.wav', '/home/audio.raw', 'file.mp3')
+        with mk.patch('voxforge2sphinx.corpus.track_files', return_value=return_list):
+            self.assertTupleEqual(
+                tuple(self.speaker.gather_audios()),
+                (('001', '/home/001.wav'), ('audio', '/home/audio.raw'), ('file', 'file.mp3')))
+
+
+class FileWriterTest(unittest.TestCase):
     def test_store(self):
         file_path = 'testpath'
         file_w = cps.FileWriter(file_path)
@@ -49,21 +60,20 @@ class FileWriterTest(unittest.TestCase):
 
     def test_add_content(self):
         text = 'text1 text2'.split()
-        trans = cps.TranscriptionFile('test')
+        trans = cps.TranscriptionWriter('test')
         trans.add_content(*text)
-        self.assertEqual(trans.content[0], cps.TranscriptionFile.FORMAT.format(*text))
-        fileid = cps.FileidFile('test')
+        self.assertEqual(trans.content[0], cps.TranscriptionWriter.FORMAT.format(*text))
+        fileid = cps.FileidWriter('test')
         fileid.add_content(*text)
-        self.assertEqual(fileid.content[0], cps.FileidFile.FORMAT.format(*text))
+        self.assertEqual(fileid.content[0], cps.FileidWriter.FORMAT.format(*text))
 
 
 class PromptsTest(unittest.TestCase):
-
-    @mk.patch('voxforge2sphinx.corpus.Corpus.track_files', return_value=['t.txt'])
+    @mk.patch('voxforge2sphinx.corpus.track_files', return_value=['t.txt'])
     def test_prompts(self, mock_track_files):
         with self.assertRaises(NotImplementedError):
             prompts = cps.Prompts()
-            prompts.load_prompts()
+            prompts.populate()
         with self.assertRaisesRegex(TypeError, 'Missing positional argument'):
             cps.Prompts.create_prompts()
         with self.assertRaisesRegex(TypeError, 'Missing.*"multi_path"'):
@@ -73,19 +83,18 @@ class PromptsTest(unittest.TestCase):
             with mk.patch('voxforge2sphinx.corpus.path.exists', return_value=False):
                 multi_prompt = cps.Prompts.create_prompts('', multi_path='')
                 self.assertIsInstance(multi_prompt, cps.MultiFilePrompts)
-                multi_prompt.load_prompts()
+                multi_prompt.populate()
                 self.assertEqual(len(multi_prompt), 0)
 
         with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(), create=True):
             with mk.patch('voxforge2sphinx.corpus.path.exists', return_value=True):
                 single_prompt = cps.Prompts.create_prompts('', multi_path='')
                 self.assertIsInstance(single_prompt, cps.SingleFilePrompts)
-                single_prompt.load_prompts()
+                single_prompt.populate()
                 self.assertEqual(len(single_prompt), 0)
 
 
 class SingleFilePromptsTest(unittest.TestCase):
-
     def setUp(self):
         self.prompts = cps.SingleFilePrompts('')
 
@@ -96,27 +105,39 @@ class SingleFilePromptsTest(unittest.TestCase):
                     '096': 'que horas são?', '097': 'amanhã é sexta.', '16': 'para onde a senhora quer ir?'}
 
         with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data=data), create=True):
-            self.prompts.load_prompts()
+            self.prompts.populate()
             self.assertEqual(self.prompts, exp_dict)
 
     def test_invalid_single(self):
         data = "094   \nPara onde a senhora quer ir?\n096Que horas são?\n\n"
         with mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data=data), create=True):
-            self.prompts.load_prompts()
+            self.prompts.populate()
             self.assertEqual(self.prompts, {})
 
 
 class MultiFilePromptsTest(unittest.TestCase):
-
-    @mk.patch('voxforge2sphinx.corpus.Corpus.track_files', return_value=['/home/user/test.txt'])
+    @mk.patch('voxforge2sphinx.corpus.track_files', return_value=['/home/user/test.txt'])
     @mk.patch('voxforge2sphinx.corpus.open', mk.mock_open(read_data="Vou tomar um pouquinho d'água"), create=True)
     def test_multi(self, mock_track_files):
         multi = cps.MultiFilePrompts('/home/user/', 'txt')
-        multi.load_prompts()
+        multi.populate()
         mock_track_files.assert_called_with('/home/user/', 'txt')
         self.assertTrue('test' in multi)
         self.assertEqual(multi['test'], "vou tomar um pouquinho d'água")
 
+
+class AudiosTest(unittest.TestCase):
+
+    def setUp(self):
+        self.audios = cps.Audios('')
+
+    def test_populate(self):
+        return_list = ('/home/001.wav', '/home/audio.raw', 'file.mp3')
+        with mk.patch('voxforge2sphinx.corpus.track_files', return_value=return_list):
+            self.audios.populate()
+            self.assertTupleEqual(
+                tuple(self.audios),
+                (('001', '/home/001.wav'), ('audio', '/home/audio.raw'), ('file', 'file.mp3')))
 
 if __name__ == "__main__":
     unittest.main()
